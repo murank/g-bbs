25f6d6a2e3fb5e9a2d4a1e7db611f18e2ddab074
murank <murank42@gmail.com>
2011-05-22 12:53:14 +0900

    accept postings from the form

--- src/gbbs/controller/PostController.java
+++ src/gbbs/controller/PostController.java
@@ -1,12 +1,28 @@
 package gbbs.controller;
 
+import gbbs.service.PostingService;
+
 import org.slim3.controller.Controller;
 import org.slim3.controller.Navigation;
 
 public class PostController extends Controller {
+	
+	private PostingService service = new PostingService();
 
 	@Override
 	public Navigation run() throws Exception {
+		
+		if(isPost() && doPost()) {
+			return redirect("/");
+		}
+		
 		return forward("post.jsp");
+	}
+	
+	private boolean doPost() {
+		String content = asString("content");
+		service.addNewPosting(content);
+		
+		return true;
 	}
 }
--- test/gbbs/controller/PostControllerTest.java
+++ test/gbbs/controller/PostControllerTest.java
@@ -1,5 +1,10 @@
 package gbbs.controller;
 
+import gbbs.model.Posting;
+
+import java.util.Date;
+
+import org.slim3.datastore.Datastore;
 import org.slim3.tester.ControllerTestCase;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -9,10 +14,32 @@ public class PostControllerTest extends ControllerTestCase {
 
 	@Test
 	public void run() throws Exception {
+		tester.param("content", "aaa");
 		tester.start("/post");
 		PostController controller = tester.getController();
 		assertThat(controller, is(notNullValue()));
 		assertThat(tester.isRedirect(), is(false));
 		assertThat(tester.getDestinationPath(), is("/post.jsp"));
+
+		Posting stored = Datastore.query(Posting.class).asSingle();
+		assertThat(stored, is(nullValue()));
+	}
+	
+	@Test
+	public void addNewPost() throws Exception {
+		String content = "aaa";
+
+		tester.request.setMethod("POST");
+		tester.param("content", content);
+		tester.start("/post");
+		PostController controller = tester.getController();
+		assertThat(controller, is(notNullValue()));
+		assertThat(tester.isRedirect(), is(true));
+		assertThat(tester.getDestinationPath(), is("/"));
+
+		Posting stored = Datastore.query(Posting.class).asSingle();
+		assertThat(stored, is(notNullValue()));
+		assertThat(stored.getContent(), is(content));
+		assertThat((new Date().getTime() - stored.getPostingDate().getTime()) <= 1 * 1000, is(true));
 	}
 }
