f0ce9d9458bd38d609bbe638696c20cb20c4948f
murank <murank42@gmail.com>
2011-05-22 13:35:28 +0900

    set postings list to requestScope

--- src/gbbs/controller/IndexController.java
+++ src/gbbs/controller/IndexController.java
@@ -1,12 +1,23 @@
 package gbbs.controller;
 
+import gbbs.model.Posting;
+import gbbs.service.PostingService;
+
+import java.util.List;
+
 import org.slim3.controller.Controller;
 import org.slim3.controller.Navigation;
 
 public class IndexController extends Controller {
 
+	private PostingService service = new PostingService();
+
 	@Override
 	public Navigation run() throws Exception {
+		
+		List<Posting> postingList = service.getPostingList();
+		requestScope("postingList", postingList);
+		
 		return forward("index.jsp");
 	}
 }
--- test/gbbs/controller/IndexControllerTest.java
+++ test/gbbs/controller/IndexControllerTest.java
@@ -15,4 +15,19 @@ public class IndexControllerTest extends ControllerTestCase {
 		assertThat(tester.isRedirect(), is(false));
 		assertThat(tester.getDestinationPath(), is("/index.jsp"));
 	}
+	
+	@Test
+	public void afterPosting() throws Exception {
+		String message = "aaa";
+
+		tester.sessionScope("message", message);
+		tester.start("/");
+		
+		IndexController controller = tester.getController();
+		assertThat(controller, is(notNullValue()));
+		assertThat(tester.isRedirect(), is(false));
+		assertThat(tester.getDestinationPath(), is("/index.jsp"));
+
+		assertThat(tester.requestScope("postingList"), is(notNullValue()));
+	}
 }
