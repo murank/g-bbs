02ebe7c7bf658f2a835b2e2d3b5491b44feaeb31
murank <murank42@gmail.com>
2011-05-22 12:28:50 +0900

    add a posting method to PostingService

--- src/gbbs/service/PostingService.java
+++ src/gbbs/service/PostingService.java
@@ -1,5 +1,26 @@
 package gbbs.service;
 
+import gbbs.model.Posting;
+
+import java.util.Date;
+
+import org.slim3.datastore.Datastore;
+
 public class PostingService {
 
+	public Posting addNewPosting(String content, Date postingDate) {
+		Posting post = new Posting();
+		post.setKey(Datastore.allocateId(Posting.class));
+		post.setContent(content);
+		post.setPostingDate(postingDate);
+
+		Datastore.put(post);
+
+		return post;
+	}
+
+	public Posting addNewPosting(String content) {
+		return addNewPosting(content, new Date());
+	}
+	
 }
--- test/gbbs/service/PostingServiceTest.java
+++ test/gbbs/service/PostingServiceTest.java
@@ -1,5 +1,10 @@
 package gbbs.service;
 
+import gbbs.model.Posting;
+
+import java.util.Date;
+
+import org.slim3.datastore.Datastore;
 import org.slim3.tester.AppEngineTestCase;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -12,5 +17,19 @@ public class PostingServiceTest extends AppEngineTestCase {
 	@Test
 	public void test() throws Exception {
 		assertThat(service, is(notNullValue()));
+	}
+
+	@Test
+	public void addNewPosting() throws Exception {
+		String content = "aaa";
+		Date postingDate = new Date();
+
+		Posting posting = service.addNewPosting(content, postingDate);
+		assertThat(posting, is(notNullValue()));
+
+		Posting stored = Datastore.get(Posting.class, posting.getKey());
+		assertThat(stored, is(notNullValue()));
+		assertThat(stored.getContent(), is(content));
+		assertThat(stored.getPostingDate(), is(postingDate));
 	}
 }
